cab8b7a593536b3db09a45e5cba2409156072a3b
konstrictor <jonkur96@gmail.com>
2020-11-04 16:40:04 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+// import Login from "./pages/Login";
+export default {
+	// components:{
+	// 	Home,
+	// 	"login-page": Login
+	// }
+};
+</script>
+
+<style src="./css/styles.css">
+
+</style>
