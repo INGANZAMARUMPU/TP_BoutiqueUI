c0c06e78a91e6c25d553d28401fca8399d6d0f60
konstrictor <jonkur96@gmail.com>
2020-11-05 20:15:55 +0200

    added item component

---
+++ src/components/item.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="item">
+		<div class="h2">
+			<span class="title">{{item.title}}</span>
+		</div>
+		<div class="qtt">{{item.qtt}}</div>
+		<div>
+			<hr>
+			<div class="button">Modifier</div>
+			<hr class="separateur">
+			<div class="button">Supprimer</div>
+		</div>
+	</div>	
+</template>
+<script>
+export default{
+	props:{
+		item:{
+			type:Object,
+			required:true
+		}
+	}
+};	
+</script>
+<style scoped>
+.item{
+	position: relative;
+	background: cadetblue;
+	border-radius: 5px;
+	width: 120px;
+	margin: 5px;
+	overflow: hidden;
+}
+*{
+	color: white;
+	text-align: center
+}
+.button{
+	width: 100%;
+	padding: 2px;
+}
+.button:hover{
+	background: #588;
+}
+.h2{
+	height: 50px;
+	font-size: 1.3em;
+	font-weight: bold;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.qtt{
+	font-size: .8em;
+	font-weight: bold;
+	color:#933;
+}
+</style>
--- src/pages/produit.vue
+++ src/pages/produit.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
+  <div class="listview">
   	<AddItem/>
+  	<Item :item="{'title':'Ibiharage Bitoto', 'qtt':'20kg'}"/>
+  	<Item :item="{'title':'Amata', 'qtt':'30 litres'}"/>
   </div>
 </template>
 
 <script>
 import AddItem from "../components/add_item"
+import Item from "../components/item"
 export default {
-	components:{
-		AddItem
-	}
+	components:{AddItem, Item},
 };
 </script>
 
 <style scoped>
-
+.listview{
+	display: flex;
+	align-items: center;
+}
 </style>
